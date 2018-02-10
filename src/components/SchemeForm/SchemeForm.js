7f4d1088e0530c31bdc0e491ad09f985df6c6e9f
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2018-02-10 22:14:39 +0300

    Fix styles for schema form

--- src/components/SchemeForm/SchemeForm.js
+++ src/components/SchemeForm/SchemeForm.js
@@ -2,52 +2,53 @@ import React from 'react';
 import Input from '../Input';
 import SchemeField from '../SchemeField/SchemeField.js';
 import Button from '../Button';
+import './SchemeForm.css';
 
 class SchemeForm extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			name: props.name || ''
-		}
-	}
+    this.state = {
+      name: props.name || ''
+    }
+  }
 
-	render() {
-		const {name, id, fields, deleteForm, addField, deleteField, saveForm, updateField} = this.props;
-		return (
-			<div className="scheme-form card">
-				<div className="card-header">
-					<a onClick={() => deleteForm(id)} className="card-header-icon" aria-label="more options">
-				<span className="icon">
-					<i className="fa fa-trash" aria-hidden="true"/>
-				</span>
-					</a>
-					<Button onClick={() => saveForm(id, this.state.name)}
-							className="is-outlined">
-						Save
-					</Button>
-				</div>
-				<div className="card-content">
-					<Input placeholder="Scheme Name" value={name} onChange={(e) => this.setState({name: e.target.value})}/>
-					{
-						fields && Object.keys(fields).map((fieldId) =>
-							<SchemeField key={fieldId}
-										 id={fieldId}
-										 deleteField={(fieldId) => deleteField(id, fieldId)}
-										 onChange={(value) => updateField(id, fieldId, value)}
-										 {...fields[fieldId]}/>
-						)
-					}
-					<Button onClick={() => addField(id)}
-							className="is-outlined"
-							iconHelpClass="is-small"
-							iconClassName="fa-plus">
-						Add Field
-					</Button>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const {name, id, fields, deleteForm, addField, deleteField, saveForm, updateField} = this.props;
+    return (
+      <div className="scheme-form card">
+        <div className="card-header">
+          <Button onClick={() => saveForm(id, this.state.name)}
+                  className="save-btn is-outlined">
+            Save
+          </Button>
+          <a onClick={() => deleteForm(id)} className="delete-icon card-header-icon" aria-label="more options">
+            <span className="icon">
+              <i className="fa fa-trash" aria-hidden="true"/>
+            </span>
+          </a>
+        </div>
+        <div className="card-content">
+          <Input placeholder="Scheme Name" value={name} onChange={(e) => this.setState({name: e.target.value})}/>
+          {
+            fields && Object.keys(fields).map((fieldId) =>
+              <SchemeField key={fieldId}
+                           id={fieldId}
+                           deleteField={(fieldId) => deleteField(id, fieldId)}
+                           onChange={(value) => updateField(id, fieldId, value)}
+                           {...fields[fieldId]}/>
+            )
+          }
+          <Button onClick={() => addField(id)}
+                  className="is-outlined"
+                  iconHelpClass="is-small"
+                  iconClassName="fa-plus">
+            Add Field
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SchemeForm;
