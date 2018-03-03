7365e525cb57d08abcb58f02166a44c98c212563
tyndria <tyndria23@gmail.com>
2018-03-03 14:31:56 +0300

    Add function for udpate scheme (different from create the one) (a little bit ugly)

--- src/redux/ducks/projectSchemas.js
+++ src/redux/ducks/projectSchemas.js
@@ -1,6 +1,5 @@
 import uuidv4 from 'uuid/v4';
-import { createSelector } from 'reselect'
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
 
 import request from '../../utils/request';
 
@@ -56,34 +55,33 @@ export const updateField = (schemeId, fieldId, field) => ({
 
 /* TODO: REFACTOR!!! */
 export const loadData = (projectId) => {
-  return (dispatch) => {
-    return request.fetch(`/api/schemas/${projectId}`, 'GET').then((schemas) => {
-    	const newSchemasState = {};
-    	const newFieldsState = {};
-      schemas.forEach((scheme) => {
-      	newSchemasState[scheme._id] = {
-      		name: scheme.name
+	return (dispatch) => {
+		return request.fetch(`/api/schemas/${projectId}`, 'GET').then((schemas) => {
+			const newSchemasState = {};
+			const newFieldsState = {};
+			schemas.forEach((scheme) => {
+				newSchemasState[scheme._id] = {
+					name: scheme.name
 				};
-      	newFieldsState[scheme._id] = {};
-      	scheme.fields.forEach(({_id, name, type}) => {
-          newFieldsState[scheme._id][_id] = {
-          	name,
+				newFieldsState[scheme._id] = {};
+				scheme.fields.forEach(({_id, name, type}) => {
+					newFieldsState[scheme._id][_id] = {
+						name,
 						type
 					}
 				})
 			});
 
-      dispatch({type: LOAD_DATA, payload: {schemas: newSchemasState, fields: newFieldsState}});
-    });
-  };
+			dispatch({type: LOAD_DATA, payload: {schemas: newSchemasState, fields: newFieldsState}});
+		});
+	};
 };
 
-/* TODO: make adding new scheme and updating the last one the same request */
 export const saveScheme = (projectId, schemeId, name) => {
 	return (dispatch, getState) => {
 		const state = getState();
 		const fields = state.projectSchemas.fields[schemeId] || [];
-		return request.fetch('/api/schemas', 'PUT', {name, schemeId, projectId, fields}).then((data) => {
+		return request.fetch('/api/schemas/save', 'PUT', {name, schemeId, projectId, fields}).then((data) => {
 			dispatch({
 				type: SAVE_SCHEME,
 				schemeId,
@@ -95,16 +93,16 @@ export const saveScheme = (projectId, schemeId, name) => {
 };
 
 const schemas = (state = DEFAULT_STATE.schemas, action) => {
-	switch(action.type) {
+	switch (action.type) {
 		case DELETE_SCHEME:
 			const {[action.id]: removedScheme, ...updatedSchemas} = state;
 			return updatedSchemas;
 		case ADD_EMPTY_SCHEME:
 			return {...state, [uuidv4()]: EMPTY_SCHEME};
 		case SAVE_SCHEME:
-			return {...state, [action.schemeId]: {name: action.name} };
-    case LOAD_DATA:
-      return action.payload.schemas;
+			return {...state, [action.schemeId]: {name: action.name}};
+		case LOAD_DATA:
+			return action.payload.schemas;
 		default:
 			return state;
 	}
@@ -112,15 +110,15 @@ const schemas = (state = DEFAULT_STATE.schemas, action) => {
 
 const fields = (state = DEFAULT_STATE.fields, action) => {
 	let updatedFields;
-	switch(action.type) {
+	switch (action.type) {
 		case ADD_EMPTY_FIELD:
-			updatedFields = {...state[action.schemeId], [uuidv4()]:EMPTY_FIELD};
+			updatedFields = {...state[action.schemeId], [uuidv4()]: EMPTY_FIELD};
 			return {...state, [action.schemeId]: updatedFields};
 		case DELETE_FIELD:
 			let {[action.fieldId]: removedField, ...updatedFields} = state[action.schemeId];
 			return {...state, [action.schemeId]: updatedFields};
 		case UPDATE_FIELD:
-			return {...state, [action.schemeId]: {...state[action.schemeId], [action.fieldId]: action.field }};
+			return {...state, [action.schemeId]: {...state[action.schemeId], [action.fieldId]: action.field}};
 		case LOAD_DATA:
 			return action.payload.fields;
 		default:
