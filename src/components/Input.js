d40f6db365346bfac38d75bdeecc5f37b4777bf7
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2018-03-11 14:13:21 +0300

    Add autofocus attr to Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,42 +2,43 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Input = ({
-				   label,
-				   type,
-				   value,
-				   hintText,
-				   successClass,
-				   leftIconClass,
-				   rightIconClass,
-				   placeholder,
-				   onChange
-}) => (
-	<div className="field">
-		{ label && <label className="label">{label}</label> }
-		<div className={classNames('control', {'has-icons-left': leftIconClass, 'has-icons-right': rightIconClass})}>
-			<input className={classNames('input', successClass)}
-				   type={type || 'text'}
-				   defaultValue={value}
-				   placeholder={placeholder}
-				   onChange={onChange}
-				   {...this.props} />
-			{
-				leftIconClass &&
-				<span className="icon is-small is-left">
-				  <i className={classNames('fa', leftIconClass)} />
+                 label,
+                 type,
+                 value,
+                 hintText,
+                 successClass,
+                 leftIconClass,
+                 rightIconClass,
+                 placeholder,
+                 onChange
+               }) => (
+  <div className="field">
+    { label && <label className="label">{label}</label> }
+    <div className={classNames('control', {'has-icons-left': leftIconClass, 'has-icons-right': rightIconClass})}>
+      <input className={classNames('input', successClass)}
+             type={type || 'text'}
+             defaultValue={value}
+             placeholder={placeholder}
+             onChange={onChange}
+             autoFocus
+             {...this.props} />
+      {
+        leftIconClass &&
+        <span className="icon is-small is-left">
+				  <i className={classNames('fa', leftIconClass)}/>
 				</span>
-			}
-			{
-				rightIconClass &&
-				<span className="icon is-small is-right">
-				  <i className={classNames('fa', rightIconClass)} />
+      }
+      {
+        rightIconClass &&
+        <span className="icon is-small is-right">
+				  <i className={classNames('fa', rightIconClass)}/>
 				</span>
-			}
-		</div>
-		{
-			hintText && <p className={classNames('help', successClass)}>{hintText}</p>
-		}
-	</div>
+      }
+    </div>
+    {
+      hintText && <p className={classNames('help', successClass)}>{hintText}</p>
+    }
+  </div>
 );
 
 export default Input;
