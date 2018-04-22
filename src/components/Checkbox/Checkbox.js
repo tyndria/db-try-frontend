ae0dbff1aba2bf8c67bc6899c6263deeca8dd31e
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2018-04-22 22:54:06 +0300

    Refactor code

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classnames from 'classnames';
 
-const Checkbox = ({onChange, label, checked, className}) => (
+import './Checkbox.css';
+
+const Checkbox = ({onChange, label, checked, className, labelClassName}) => (
   <div className={classnames('field', className)}>
-    <label className="checkbox">
+    <label className={classnames('checkbox', labelClassName)}>
       <input type="checkbox" onChange={onChange} checked={checked} />
       {label}
     </label>
