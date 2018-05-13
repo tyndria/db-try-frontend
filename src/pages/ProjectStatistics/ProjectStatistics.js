639f66cb6e4a88f7955f4bd4d45539ea0edb166f
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2018-05-13 14:56:59 +0300

    Add the simplest loader to the statistics page

--- src/pages/ProjectStatistics/ProjectStatistics.js
+++ src/pages/ProjectStatistics/ProjectStatistics.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+import './ProjectStatistics.css';
 
 const ProjectStatistics = ({chartData}) => (
-	<ResponsiveContainer width="100%" height="100%">
-		<BarChart data={chartData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-			<XAxis dataKey="operation"/>
-			<YAxis/>
-			<CartesianGrid strokeDasharray="3 3"/>
-			<Tooltip/>
-			<Legend />
-			<Bar dataKey="mongodb" fill="#8884d8" />
-			<Bar dataKey="mysql" fill="#82ca9d" />
-		</BarChart>
-	</ResponsiveContainer>
+	<div className="chart-container">
+		<ResponsiveContainer width="100%" height="100%">
+			<BarChart data={chartData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+				<XAxis dataKey="operation"/>
+				<YAxis />
+				<CartesianGrid strokeDasharray="3 3" />
+				<Tooltip />
+				<Legend align="left"/>
+				<Bar dataKey="mongodb" fill="#8884d8" />
+				<Bar dataKey="mysql" fill="#82ca9d" />
+			</BarChart>
+		</ResponsiveContainer>
+		{!chartData.length && <div className="loader" /> }
+	</div>
 );
 
 const mapStateToProps = state => ({
--- src/redux/ducks/projectStatistics.js
+++ src/redux/ducks/projectStatistics.js
@@ -1,12 +1,14 @@
 import request from '../../utils/request';
 
 export const RUN_PROJECT = 'RUN_PROJECT';
+export const RUN_PROJECT_SUCCESS = 'RUN_PROJECT_SUCCESS';
 
 export const runProject = (projectId) => {
   return (dispatch, getState) => {
   	const configs = getState().schemasConfiguration[projectId];
+  	dispatch({type: RUN_PROJECT});
     return request.fetch(`/api/projects/run/${projectId}`, 'POST', configs).then((data) => {
-      dispatch({type: RUN_PROJECT, payload: data});
+      dispatch({type: RUN_PROJECT_SUCCESS, payload: data});
     });
   };
 };
@@ -18,7 +20,10 @@ const DEFAULT_STATE = {
 
 export default (state = DEFAULT_STATE, action) => {
 	switch(action.type) {
-		case RUN_PROJECT:
+		case RUN_PROJECT: {
+			return {...state, data: []};
+		}
+		case RUN_PROJECT_SUCCESS:
 			return {...state, data: action.payload};
 		default:
 			return state;
