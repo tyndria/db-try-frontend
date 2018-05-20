b38c4aedd758b2d46583d48332a9ef58b751a885
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2018-05-20 16:45:51 +0300

    Add search input for logs

--- src/components/LogItem/LogItem.js
+++ src/components/LogItem/LogItem.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './LogItem.css';
 
-const LogItem = ({coll, doc, method, query, timestamp}) => (
-  <div className="log">
-    <span className="timestamp">{timestamp}{' '}</span>
-    <span className="collection">{coll}{' '}</span>
-    <span className="method">{method}{' '}</span>
-    <span>{query}{' '}</span>
-    <span>{doc}</span>
-  </div>
-);
+class LogItem extends Component {
+    render() {
+        const {timestamp, coll, method, query: queryString} = this.props;
+        const query = JSON.parse(queryString);
+        return (
+          <div className="log">
+              <span className="timestamp">{timestamp}{' '}</span>
+              <span className="collection">{coll}{' '}</span>
+              <span className="method">{method}{' '}</span>
+              <span>{Object.keys(query).map((key) =>
+                <div><span>{key}{': '}</span><span>{query[key].toString()}</span></div>
+              )}{' '}</span>
+          </div>
+        );
+    }
+};
 
 export default LogItem;
--- src/components/Logs/Logs.js
+++ src/components/Logs/Logs.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {DebounceInput} from 'react-debounce-input';
 import Button from '../Button';
 import LogItem from '../LogItem/LogItem';
 
@@ -8,17 +9,34 @@ import {getLogs} from '../../redux/ducks/logs';
 import './Logs.css'
 
 class Logs extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      logToSearch: ''
+    };
+  }
   render() {
     const {logs, handleLoadLogs, projectId} = this.props
+    const filteredSearch = logs.filter((log) => {
+      return JSON.stringify(log).search(this.state.logToSearch) !== -1;
+    });
     return (
       <div className="logs">
-        <div className="list">
-          {logs.map((log) =>
-            <LogItem {...log}/>
-          )}
-        </div>
+        {!!logs.length && <DebounceInput
+          className="search"
+          minLength={3}
+          debounceTimeout={300}
+          placeholder="Search..."
+          onChange={({target: {value}}) => this.setState({logToSearch: value})} /> }
+        {!!logs.length &&
+          <div className="list">
+            {filteredSearch.map((log) =>
+              <LogItem {...log}/>
+            )}
+          </div>}
 
-        {(!logs || !logs.length) &&
+        {!logs.length &&
           <Button className="load-logs is-light" onClick={() => handleLoadLogs(projectId)}>Load logs</Button>
         }
       </div>
